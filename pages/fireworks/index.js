4b708bc7009f8e30ba0cc6544d3ce5dd04f5a7a9
jinbaojia <bjjins@163.com>
2025-07-15 13:54:57 +0800

    feat/fireworks: 优化烟花效果并添加天空盒
    - 移除 OrbitControls 和 AxesHelper
    - 添加 Sky 组件，实现动态天空盒效果
    - 改进烟花算法，增加随机性和多样性
    - 优化着色器代码，添加烟花绽放和闪烁效果
    - 调整相机设置和渲染逻辑

--- pages/fireworks/index.js
+++ pages/fireworks/index.js
@@ -1,6 +1,6 @@
 import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/Addons.js";
-import { camera, canvas, renderer, scene, size as winSize, textureLoader } from "../template";
+import { OrbitControls, Sky } from "three/examples/jsm/Addons.js";
+import { camera, canvas, renderer, scene, size as winSize, textureLoader ,gui} from "../template";
 import vertexShader from "./shaders/vertex.glsl";
 import fragmentShader from "./shaders/fragment.glsl";
 import gsap from "gsap";
@@ -8,9 +8,8 @@ import gsap from "gsap";
 
 camera.position.set(1.5, 0, 6);
 camera.lookAt(0, 0, 0);
-const controls = new OrbitControls(camera, canvas);
-controls.enableDamping = true;
-scene.add(new THREE.AxesHelper(1));
+// const controls = new OrbitControls(camera, canvas);
+// controls.enableDamping = true;
 
 
 const textures = [
@@ -27,6 +26,57 @@ const textures = [
 
 const fireworkPosition = new THREE.Vector3();
 const spherical = new THREE.Spherical();
+
+const sky = new Sky();
+sky.scale.setScalar(450000);
+scene.add(sky);
+
+const sun = new THREE.Vector3();
+
+const effectController = {
+    turbidity: 10,
+    rayleigh: 3,
+    mieCoefficient: 0.005,
+    mieDirectionalG: 0.7,
+    elevation: 2,
+    azimuth: 180,
+    exposure: renderer.toneMappingExposure
+};
+
+function guiChanged() {
+
+    const uniforms = sky.material.uniforms;
+    uniforms['turbidity'].value = effectController.turbidity;
+    uniforms['rayleigh'].value = effectController.rayleigh;
+    uniforms['mieCoefficient'].value = effectController.mieCoefficient;
+    uniforms['mieDirectionalG'].value = effectController.mieDirectionalG;
+
+    const phi = THREE.MathUtils.degToRad(90 - effectController.elevation);
+    const theta = THREE.MathUtils.degToRad(effectController.azimuth);
+
+    sun.setFromSphericalCoords(1, phi, theta);
+
+    uniforms['sunPosition'].value.copy(sun);
+
+    renderer.toneMappingExposure = effectController.exposure;
+    renderer.render(scene, camera);
+
+}
+
+gui.add(effectController, 'turbidity', 0.0, 20.0, 0.1).onChange(guiChanged);
+gui.add(effectController, 'rayleigh', 0.0, 4, 0.001).onChange(guiChanged);
+gui.add(effectController, 'mieCoefficient', 0.0, 0.1, 0.001).onChange(guiChanged);
+gui.add(effectController, 'mieDirectionalG', 0.0, 1, 0.001).onChange(guiChanged);
+gui.add(effectController, 'elevation', 0, 90, 0.1).onChange(guiChanged);
+gui.add(effectController, 'azimuth', - 180, 180, 0.1).onChange(guiChanged);
+gui.add(effectController, 'exposure', 0, 1, 0.0001).onChange(guiChanged);
+
+guiChanged();
+
+
+
+
+
 /**
  * 创建一个烟花
  * @param {number} count 烟花中粒子的数量
@@ -35,8 +85,9 @@ const spherical = new THREE.Spherical();
  * @param {number} radius 烟花半径
  * @param {THREE.Vector3} color 烟花颜色
  */
-const createFireworks = (count, position, size, texture, radius, color) => {
+const createFirework = (count, position, size, texture, radius, color) => {
     const positionsArray = new Float32Array(count * 3);
+    const timeMultipliersArray = new Float32Array(count);
     const sizesArray = new Float32Array(count);
 
     for (let i = 0; i < count; i++) {
@@ -49,6 +100,8 @@ const createFireworks = (count, position, size, texture, radius, color) => {
         positionsArray[i3 + 1] = fireworkPosition.y;
         positionsArray[i3 + 2] = fireworkPosition.z;
 
+        timeMultipliersArray[i] = 1 + Math.random();
+
         // 每个粒子的大小
         sizesArray[i] = Math.random();
 
@@ -56,6 +109,7 @@ const createFireworks = (count, position, size, texture, radius, color) => {
     const geometry = new THREE.BufferGeometry();
     geometry.setAttribute("position", new THREE.BufferAttribute(positionsArray, 3));
     geometry.setAttribute("aSize", new THREE.BufferAttribute(sizesArray, 1));
+    geometry.setAttribute('aTimeMultiplier', new THREE.Float32BufferAttribute(timeMultipliersArray, 1));
 
 
     // 翻转纹理，纹理的位置上下颠倒
@@ -92,20 +146,25 @@ const createFireworks = (count, position, size, texture, radius, color) => {
     });
 };
 
-
-window.addEventListener("click", () => {
-    createFireworks(
-        1000,
-        new THREE.Vector3(0, 0, 0),
-        0.2,
-        textures[7],
-        1,
-        new THREE.Vector3(1.0, 0.0, 1.0)
+const createRandomFirework = () => {
+    const count = Math.round(400 + Math.random() * 1000);
+    const position = new THREE.Vector3(
+        (Math.random() - 0.5) * 3,
+        Math.random(),
+        (Math.random() - 0.5) * 3
     );
-});
+    const size = 0.1 + Math.random() * 0.1;
+    const texture = textures[Math.floor(Math.random() * textures.length)];
+    const radius = 2 + Math.random();
+    const color = new THREE.Color();
+    color.setHSL(Math.random(), 1, 0.7);
+    createFirework(count, position, size, texture, radius, color);
+};
+
+
+window.addEventListener("click", createRandomFirework);
 
 function render() {
-    controls.update();
     renderer.render(scene, camera);
     requestAnimationFrame(render);
 }
