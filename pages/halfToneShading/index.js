3edd3d5ab0b622b2f95a2a82defe81c206cd732f
jinbaojia <bjjins@163.com>
2025-07-16 22:25:40 +0800

    feat(halfToneShading): 添加半色调阴影页面
    - 创建半色调阴影页面的主组件
    - 设置相机、控件和渲染器
    - 添加材质参数和 GUI 控制
    - 创建和添加几何体对象
    - 加载 Suzanne 模型并应用材质
    - 实现动画渲染逻辑

---
+++ pages/halfToneShading/index.js
@@ -0,0 +1,90 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/Addons.js";
+import { camera, canvas, renderer, scene, gltfLoader, gui } from "../template";
+import suzannePath from "../../models/suzanne.glb?url";
+import vertexShader from "./shaders/vertex.glsl";
+import fragmentShader from "./shaders/fragment.glsl";
+
+camera.position.set(7, 7, 7);
+camera.lookAt(0, 0, 0);
+
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+const rendererParameters = {};
+rendererParameters.clearColor = '#26132f';
+renderer.setClearColor(rendererParameters.clearColor);
+
+gui
+    .addColor(rendererParameters, 'clearColor')
+    .onChange(() => {
+        renderer.setClearColor(rendererParameters.clearColor);
+    });
+
+
+const materialParameters = {};
+materialParameters.color = '#ff794d';
+
+const material = new THREE.ShaderMaterial({
+    vertexShader,
+    fragmentShader,
+    uniforms:
+    {
+        uColor: new THREE.Uniform(new THREE.Color(materialParameters.color)),
+        uShadeColor: new THREE.Uniform(new THREE.Color(materialParameters.shadeColor)),
+    }
+});
+
+gui
+    .addColor(materialParameters, 'color')
+    .onChange(() => {
+        material.uniforms.uColor.value.set(materialParameters.color);
+    });
+
+const torusKnot = new THREE.Mesh(
+    new THREE.TorusKnotGeometry(0.6, 0.25, 128, 32),
+    material
+);
+torusKnot.position.x = 3;
+scene.add(torusKnot);
+
+const sphere = new THREE.Mesh(
+    new THREE.SphereGeometry(),
+    material
+);
+sphere.position.x = - 3;
+scene.add(sphere);
+
+let suzanne = null;
+gltfLoader.load(
+    suzannePath,
+    (gltf) => {
+        suzanne = gltf.scene;
+        suzanne.traverse((child) => {
+            if (child.isMesh)
+                child.material = material;
+        });
+        scene.add(suzanne);
+    }
+);
+
+const clock = new THREE.Clock()
+function render() {
+    const elapsedTime = clock.getElapsedTime();
+
+    // Rotate objects
+    if (suzanne) {
+        suzanne.rotation.x = - elapsedTime * 0.1;
+        suzanne.rotation.y = elapsedTime * 0.2;
+    }
+
+    sphere.rotation.x = - elapsedTime * 0.1;
+    sphere.rotation.y = elapsedTime * 0.2;
+
+    torusKnot.rotation.x = - elapsedTime * 0.1;
+    torusKnot.rotation.y = elapsedTime * 0.2;
+    controls.update();
+    renderer.render(scene, camera);
+    requestAnimationFrame(render);
+}
+render();
