37540d0a7bf19a65935da22a29192d1daa72d114
jinbaojia <bjjins@163.com>
2025-07-17 13:57:54 +0800

    feat(halfToneShading): 实现网点渲染效果
    - 新增 halftone 函数实现网点渲染
    - 添加光源颜色和重复次数的控制
    - 优化 fragment shader 以支持网点效果
    - 更新页面以显示新的渲染效果

--- pages/halfToneShading/index.js
+++ pages/halfToneShading/index.js
@@ -1,6 +1,6 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/Addons.js";
-import { camera, canvas, renderer, scene, gltfLoader, gui } from "../template";
+import { camera, canvas, renderer, scene, gltfLoader, gui, size as winSize } from "../template";
 import suzannePath from "../../models/suzanne.glb?url";
 import vertexShader from "./shaders/vertex.glsl";
 import fragmentShader from "./shaders/fragment.glsl";
@@ -13,6 +13,7 @@ controls.enableDamping = true;
 
 const rendererParameters = {};
 rendererParameters.clearColor = '#26132f';
+
 renderer.setClearColor(rendererParameters.clearColor);
 
 gui
@@ -24,14 +25,17 @@ gui
 
 const materialParameters = {};
 materialParameters.color = '#ff794d';
-
+materialParameters.shadowColor = '#8e19b8';
+materialParameters.lightColor = '#e5ffe0';
 const material = new THREE.ShaderMaterial({
     vertexShader,
     fragmentShader,
     uniforms:
     {
         uColor: new THREE.Uniform(new THREE.Color(materialParameters.color)),
-        uShadeColor: new THREE.Uniform(new THREE.Color(materialParameters.shadeColor)),
+        uResolution: new THREE.Uniform(winSize.resolution),
+        uLightRepetitions: new THREE.Uniform(130),
+        uLightColor: new THREE.Uniform(new THREE.Color(materialParameters.lightColor))
     }
 });
 
@@ -39,6 +43,20 @@ gui
     .addColor(materialParameters, 'color')
     .onChange(() => {
         material.uniforms.uColor.value.set(materialParameters.color);
+    });
+
+
+gui
+    .add(material.uniforms.uLightRepetitions, 'value')
+    .min(1)
+    .max(300)
+    .step(1)
+    .name("repetitions");
+
+gui
+    .addColor(materialParameters, 'lightColor')
+    .onChange(() => {
+        material.uniforms.uLightColor.value.set(materialParameters.lightColor);
     });
 
 const torusKnot = new THREE.Mesh(
@@ -68,7 +86,7 @@ gltfLoader.load(
     }
 );
 
-const clock = new THREE.Clock()
+const clock = new THREE.Clock();
 function render() {
     const elapsedTime = clock.getElapsedTime();
 
