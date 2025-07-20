de8465eb39893b0b86667c440b72465acdd9d638
jinbaojia <bjjins@163.com>
2025-07-20 21:42:33 +0800

    refactor(earth): 重构地球页面
    - 优化了地球和大气层的渲染逻辑
    - 移除了不必要的调试代码
    - 调整了相机位置和视角
    - 更新了着色器代码，提高了渲染性能
    - 增加了太阳模型，增强了场景效果

--- pages/earth/index.js
+++ pages/earth/index.js
@@ -1,42 +1,41 @@
 import * as THREE from "three";
 import { camera, gui, renderer, scene, canvas, textureLoader } from "../template";
 import { OrbitControls } from "three/examples/jsm/Addons.js";
-import vertexShader from "./shaders/vertex.glsl";
-import fragmentShader from "./shaders/fragment.glsl";
-import atmosphereVertexShader from "./shaders/atmosphereVertex.glsl";
-import atmosphereFragmentShader from "./shaders/atmosphereFragment.glsl";
+import earthVertexShader from "./vertex.glsl";
+import earthFragmentShader from "./fragment.glsl";
+import day from "./static/earth/day.jpg?url";
+import night from "./static/earth/night.jpg?url";
+import specularClouds from "./static/earth/specularClouds.jpg?url";
+import atmosphereVertexShader from "./atmosphereVertex.glsl";
+import atmosphereFragmentShader from "./atmosphereFragment.glsl";
 
-
-const dayTexture = textureLoader.load(import.meta.resolve("./static/earth/day.jpg"));
+const dayTexture = textureLoader.load(day);
+const nightTexture = textureLoader.load(night);
+const specularCloudsTexture = textureLoader.load(specularClouds);
 dayTexture.colorSpace = THREE.SRGBColorSpace;
-const nightTexture = textureLoader.load(import.meta.resolve("./static/earth/night.jpg"));
 nightTexture.colorSpace = THREE.SRGBColorSpace;
-const cloudTexture = textureLoader.load(import.meta.resolve("./static/earth/specularClouds.jpg"));
-cloudTexture.anisotropy = dayTexture.anisotropy = nightTexture.anisotropy = 8;
+
+dayTexture.anisotropy = 8;
+nightTexture.anisotropy = 8;
+
+scene.add(new THREE.AxesHelper(5));
 renderer.setClearColor('#333333')
 scene.add(new THREE.AxesHelper(5));
-camera.position.x = 12;
+camera.position.x = 4;
 camera.position.y = 5;
 camera.position.z = 4;
 camera.lookAt(0, 0, 0);
+
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+const clock = new THREE.Clock();
 
 const earthParameters = {
     atmosphereDayColor: "#00aaff",
     atmosphereTwilightColor: '#ff6600'
 };
 
-const sunSpherical = new THREE.Spherical(1, Math.PI * 0.5, 0.5);
-const sunDirection = new THREE.Vector3();
-const debugSun = new THREE.Mesh(
-    new THREE.IcosahedronGeometry(0.1, 2),
-    new THREE.MeshBasicMaterial()
-);
-scene.add(debugSun);
-
-const controls = new OrbitControls(camera, canvas);
-controls.enableDamping = true;
-const clock = new THREE.Clock();
-
+// 大气层
 const atmosphereMaterial = new THREE.ShaderMaterial({
     transparent: true,
     side: THREE.BackSide,
@@ -49,20 +48,52 @@ const atmosphereMaterial = new THREE.ShaderMaterial({
     },
 });
 
+
+// 太阳
+const sun = new THREE.Mesh(
+    new THREE.SphereGeometry(0.2),
+    new THREE.MeshBasicMaterial()
+);
+scene.add(sun);
+const sunSpherical = new THREE.Spherical(1, Math.PI / 2, 0);
+const sunDirection = new THREE.Vector3();
+const updateSun = () => {
+    sunDirection.setFromSpherical(sunSpherical);
+    sun.position.copy(sunDirection);
+    sun.position.multiplyScalar(5);
+    atmosphereMaterial.uniforms.uSunDirection.value.copy(sunDirection);
+};
+updateSun();
+
+gui.add(sunSpherical, "phi")
+    .min(0)
+    .max(Math.PI)
+    .step(0.1)
+    .onChange(updateSun);
+
+gui.add(sunSpherical, "theta")
+    .min(0)
+    .max(Math.PI * 2)
+    .step(0.1)
+    .onChange(updateSun);
+
+// 地球
 const earthGeometry = new THREE.SphereGeometry(2, 64, 64);
-const earthMaterial = new THREE.ShaderMaterial({
-    vertexShader,
-    fragmentShader,
-    uniforms: {
-        uDayTexture: new THREE.Uniform(dayTexture),
-        uNightTexture: new THREE.Uniform(nightTexture),
-        uCloudTexture: new THREE.Uniform(cloudTexture),
-        uSunDirection: new THREE.Uniform(new THREE.Vector3(0, 0, 1)),
-        uAtmosphereDayColor: new THREE.Uniform(new THREE.Color(earthParameters.atmosphereDayColor)),
-        uAtmosphereTwilightColor: new THREE.Uniform(new THREE.Color(earthParameters.atmosphereTwilightColor))
-    }
-});
-const earth = new THREE.Mesh(earthGeometry, earthMaterial);
+const earth = new THREE.Mesh(
+    earthGeometry,
+    new THREE.ShaderMaterial({
+        vertexShader: earthVertexShader,
+        fragmentShader: earthFragmentShader,
+        uniforms: {
+            uDayTexture: new THREE.Uniform(dayTexture),
+            uNightTexture: new THREE.Uniform(nightTexture),
+            uSpecularCloudsTexture: new THREE.Uniform(specularCloudsTexture),
+            uSunDirection: new THREE.Uniform(sunDirection),
+            uAtmosphereDayColor: new THREE.Uniform(new THREE.Color(earthParameters.atmosphereDayColor)),
+            uAtmosphereTwilightColor: new THREE.Uniform(new THREE.Color(earthParameters.atmosphereTwilightColor))
+        }
+    })
+);
 scene.add(earth);
 
 const atmosphere = new THREE.Mesh(
@@ -72,49 +103,14 @@ const atmosphere = new THREE.Mesh(
 atmosphere.scale.set(1.04, 1.04, 1.04);
 scene.add(atmosphere);
 
-const updateSun = () => {
-    sunDirection.setFromSpherical(sunSpherical);
-    debugSun.position
-        .copy(sunDirection)
-        .multiplyScalar(5);
-
-    earthMaterial.uniforms.uSunDirection.value.copy(sunDirection);
-    atmosphereMaterial.uniforms.uSunDirection.value.copy(sunDirection);
-
-};
-
-updateSun();
-
-gui.add(sunSpherical, 'phi')
-    .min(0)
-    .max(Math.PI)
-    .onChange(updateSun);
-
-gui.add(sunSpherical, 'theta')
-    .min(- Math.PI)
-    .max(Math.PI)
-    .onChange(updateSun);
-
-gui.addColor(earthParameters, 'atmosphereDayColor')
+gui.addColor(earthParameters, "atmosphereDayColor")
     .onChange(() => {
-        earthMaterial.uniforms.uAtmosphereDayColor.value.set(earthParameters.atmosphereDayColor);
+        earth.material.uniforms.uAtmosphereDayColor.value.set(earthParameters.atmosphereDayColor);
     });
 
-gui.addColor(earthParameters, 'atmosphereTwilightColor')
+gui.addColor(earthParameters, "atmosphereTwilightColor")
     .onChange(() => {
-        earthMaterial.uniforms.uAtmosphereTwilightColor.value.set(earthParameters.atmosphereTwilightColor);
-    });
-
-gui.addColor(earthParameters, 'atmosphereDayColor')
-    .onChange(() => {
-        earthMaterial.uniforms.uAtmosphereDayColor.value.set(earthParameters.atmosphereDayColor);
-        atmosphereMaterial.uniforms.uAtmosphereDayColor.value.set(earthParameters.atmosphereDayColor);
-    });
-
-gui.addColor(earthParameters, 'atmosphereTwilightColor')
-    .onChange(() => {
-        earthMaterial.uniforms.uAtmosphereTwilightColor.value.set(earthParameters.atmosphereTwilightColor);
-        atmosphereMaterial.uniforms.uAtmosphereTwilightColor.value.set(earthParameters.atmosphereTwilightColor);
+        earth.material.uniforms.uAtmosphereTwilightColor.value.set(earthParameters.atmosphereTwilightColor);
     });
 
 function render() {
