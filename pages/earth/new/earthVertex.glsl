f3694952fb526e21add5847a797dbfff0ea4d559
jinbaojia <bjjins@163.com>
2025-07-21 23:05:12 +0800

    refactor(earth): 删除地球着色器和渲染代码
    - 移除了 earthFragment.glsl 和 earthVertex.glsl 文件
    - 删除了 index.js 中的地球渲染相关代码
    - 清理了导入的纹理和创建的地球网格

--- pages/earth/new/earthVertex.glsl
+++
@@ -1,8 +0,0 @@
-varying vec3 vNormal;
-varying vec2 vUv;
-
-void main(){
-    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-    vNormal = (modelMatrix * vec4(normal, 0.0)).xyz;
-    vUv = uv;
-}
