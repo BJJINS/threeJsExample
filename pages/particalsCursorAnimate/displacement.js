cf213112ca44298551011e636c91aaf3ca137bd5
jinbaojia <bjjins@163.com>
2025-07-23 17:57:06 +0800

    feat(particalsCursorAnimate): 实现粒子动画与光流效果
    - 更新 displacement.js 中的画布尺寸和绘制逻辑
    - 在 index.js 中添加新纹理和粒子几何属性
    - 修改 particlesVertex.glsl 中的顶点计算，增加置换效果
    - 在 .vscode/settings.json 中添加 "smoothstep" 关键字

--- pages/particalsCursorAnimate/displacement.js
+++ pages/particalsCursorAnimate/displacement.js
@@ -1,9 +1,9 @@
-import { Mesh, MeshBasicMaterial, PlaneGeometry } from "three";
+import { CanvasTexture, DoubleSide, Mesh, MeshBasicMaterial, PlaneGeometry } from "three";
 import glow from "./static/glow.png?url";
 const canvas = document.createElement("canvas");
 canvas.style.position = 'fixed';
-canvas.style.width = '512px';
-canvas.style.height = '512px';
+canvas.style.width = '256px';
+canvas.style.height = '256px';
 canvas.style.top = 0;
 canvas.style.left = 0;
 canvas.style.zIndex = 2;
@@ -11,6 +11,7 @@ canvas.width = canvas.height = 128;
 const context = canvas.getContext("2d");
 const image = new Image();
 image.src = glow;
+context.globalCompositeOperation = 'lighten';
 context.fillRect(0, 0, canvas.width, canvas.height);
 document.body.appendChild(canvas);
 
@@ -18,15 +19,20 @@ document.body.appendChild(canvas);
 const interactivePlane = new Mesh(
     new PlaneGeometry(10, 10),
     new MeshBasicMaterial({
-        color: "red"
+        side: DoubleSide
     })
 );
+
+interactivePlane.visible = false;
+
+const texture = new CanvasTexture(canvas);
 
 
 export default {
     context,
     interactivePlane,
     image,
-    canvas
+    canvas,
+    texture
 }
 
--- pages/particalsCursorAnimate/index.js
+++ pages/particalsCursorAnimate/index.js
@@ -3,10 +3,10 @@ import { camera, renderer, scene, canvas, size, textureLoader } from "../templat
 import { OrbitControls } from "three/examples/jsm/Addons.js";
 import particlesVertexShader from "./particlesVertex.glsl";
 import particlesFragmentShader from "./particlesFragment.glsl";
-import dog from "./static/picture-1.png?url";
+import picture from "./static/picture-4.png?url";
 import displacement from "./displacement";
 
-const dogTexture = textureLoader.load(dog);
+const dogTexture = textureLoader.load(picture);
 camera.fov = 35;
 camera.far = 100;
 camera.updateProjectionMatrix();
@@ -18,19 +18,28 @@ controls.enableDamping = true;
 scene.add(displacement.interactivePlane);
 
 const particlesGeometry = new THREE.PlaneGeometry(10, 10, 128, 128);
+particlesGeometry.setIndex(null);
+particlesGeometry.deleteAttribute("normal");
+const intensitiesArray = new Float32Array(particlesGeometry.attributes.position.count);
+const anglesArray = new Float32Array(particlesGeometry.attributes.position.count);
+for (let i = 0; i < particlesGeometry.attributes.position.count; i++) {
+    intensitiesArray[i] = Math.random();
+    anglesArray[i] = Math.random() * 2 * Math.PI;
+}
+particlesGeometry.setAttribute("aIntensity", new THREE.BufferAttribute(intensitiesArray, 1));
+particlesGeometry.setAttribute("aAngle", new THREE.BufferAttribute(anglesArray, 1));
 const particlesMaterial = new THREE.ShaderMaterial({
     vertexShader: particlesVertexShader,
     fragmentShader: particlesFragmentShader,
     uniforms: {
         uResolution: new THREE.Uniform(size.resolution),
         uPictureTexture: new THREE.Uniform(dogTexture),
+        uDisplacementTexture: new THREE.Uniform(displacement.texture)
     }
 });
 
 const particles = new THREE.Points(particlesGeometry, particlesMaterial);
 scene.add(particles);
-
-
 
 const rayCaster = new THREE.Raycaster();
 const screenCursor = new THREE.Vector2(9999, 9999);
@@ -50,14 +59,21 @@ function render() {
         canvasCursor.x = uv.x * displacement.canvas.width;
         canvasCursor.y = (1 - uv.y) * displacement.canvas.height;
     }
-    displacement.context.clearRect(0, 0, displacement.canvas.width, displacement.canvas.height);
+    displacement.context.globalCompositeOperation = "source-over";
+    displacement.context.globalAlpha = 0.02;
+    displacement.context.fillRect(0, 0, displacement.canvas.width, displacement.canvas.height);
+    displacement.context.globalCompositeOperation = 'lighten';
+    displacement.context.globalAlpha = 1;
+    const glowSize = displacement.canvas.width * 0.25;
     displacement.context.drawImage(
         displacement.image,
-        canvasCursor.x,
-        canvasCursor.y,
-        32,
-        32
+        canvasCursor.x - glowSize / 2,
+        canvasCursor.y - glowSize / 2,
+        glowSize,
+        glowSize
     );
+
+    displacement.texture.needsUpdate = true;
 
     renderer.render(scene, camera);
     requestAnimationFrame(render);
