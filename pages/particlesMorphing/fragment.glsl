4d7a16b9732de60e91ac8f4b6f7bc6b00fd03e8a
jinbaojia <bjjins@163.com>
2025-07-25 22:16:35 +0800

    feat(particlesMorphing): 实现粒子变形效果
    - 添加颜色变换和粒子大小属性
    - 实现基于模型的粒子位置变换
    - 增加噪声处理以实现更自然的变形效果
    - 添加 GUI 控制变形进度和颜色

--- pages/particlesMorphing/fragment.glsl
+++ pages/particlesMorphing/fragment.glsl
@@ -1,10 +1,11 @@
+varying vec3 v_color;
 void main() {
     vec2 uv = gl_PointCoord;
     float distanceToCenter = distance(uv, vec2(0.5));
     float alpha = 0.05 / distanceToCenter - 0.1;
 
 
-    gl_FragColor = vec4(1.0,1.0,1.0, alpha);
+    gl_FragColor = vec4(v_color, alpha);
     #include <tonemapping_fragment>
     #include <colorspace_fragment>
 }
--- pages/particlesMorphing/index.js
+++ pages/particlesMorphing/index.js
@@ -7,10 +7,49 @@ import fragmentShader from "./fragment.glsl";
 import model from "./static/models.glb?url";
 
 useDraco();
+scene.add(new THREE.AxesHelper(4));
 gltfLoader.load(model, (gltf) => {
-    scene.add(gltf.scene);
+    let maxCount = 0;
+    const newPositions = [];
+    let currentIndex = 0;
+    const colorA = '#ff7300';
+    const colorB = '#0091ff';
 
-    const geometry = new THREE.SphereGeometry(3);
+
+    const positions = gltf.scene.children.map((child) => {
+        const { position } = child.geometry.attributes;
+        const { count } = position;
+        maxCount = Math.max(count, maxCount);
+        return position;
+    });
+
+    for (const position of positions) {
+        const originalArray = position.array;
+        const newArray = new Float32Array(maxCount * 3);
+        for (let i = 0; i < maxCount; i++) {
+            const i3 = i * 3;
+            if (i3 < originalArray.length) {
+                newArray[i3] = originalArray[i3];
+                newArray[i3 + 1] = originalArray[i3 + 1];
+                newArray[i3 + 2] = originalArray[i3 + 2];
+            } else {
+                const randomIndex = Math.floor(Math.random() * position.count) * 3;
+                newArray[i3] = originalArray[randomIndex];
+                newArray[i3 + 1] = originalArray[randomIndex + 1];
+                newArray[i3 + 2] = originalArray[randomIndex + 2];
+            }
+        }
+        newPositions.push(new THREE.Float32BufferAttribute(newArray, 3));
+    }
+
+    const sizesArray = new Float32Array(maxCount);
+    for (let i = 0; i < maxCount; i++)
+        sizesArray[i] = Math.random();
+
+    const geometry = new THREE.BufferGeometry();
+    geometry.setAttribute("position", newPositions[currentIndex]);
+    geometry.setAttribute("a_positionTarget", newPositions[1]);
+    geometry.setAttribute('a_size', new THREE.BufferAttribute(sizesArray, 1));
     geometry.setIndex(null);
     const material = new THREE.ShaderMaterial({
         vertexShader,
@@ -19,12 +58,50 @@ gltfLoader.load(model, (gltf) => {
         depthWrite: false,
         uniforms: {
             u_size: new THREE.Uniform(0.4),
-            u_resolution: new THREE.Uniform(size.resolution)
+            u_resolution: new THREE.Uniform(size.resolution),
+            u_progress: new THREE.Uniform(0),
+            u_colorA: new THREE.Uniform(new THREE.Color(colorA)),
+            u_colorB: new THREE.Uniform(new THREE.Color(colorB))
         }
     });
 
+    const aPoints = new THREE.Points(geometry, material)
+    scene.add(aPoints)
+    console.log(aPoints.geometry.boundingSphere)
+
+
+
+    const morph = (index) => {
+        geometry.setAttribute("position", newPositions[currentIndex]);
+        geometry.setAttribute("a_positionTarget", newPositions[index]);
+        gsap.fromTo(material.uniforms.u_progress,
+            { value: 0 },
+            {
+                value: 1,
+                duration: 3,
+                ease: "power1.out",
+            });
+        currentIndex = index;
+    };
+
+
+    const morphFnMap = {
+        morph0: () => { morph(0); },
+        morph1: () => { morph(1); },
+        morph2: () => { morph(2); },
+        morph3: () => { morph(3); },
+    };
+
+    gui.add(morphFnMap, 'morph0');
+    gui.add(morphFnMap, 'morph1');
+    gui.add(morphFnMap, 'morph2');
+    gui.add(morphFnMap, 'morph3');
+
     const points = new THREE.Points(geometry, material);
     scene.add(points);
+
+    gui.add(material.uniforms.u_progress, "value", 0, 1, 0.01).name("u_progress").listen();
+
 });
 
 
@@ -35,7 +112,6 @@ camera.position.set(0, 0, 16);
 camera.lookAt(0.0);
 const orbitControl = new OrbitControls(camera, renderer.domElement);
 orbitControl.enableDamping = true;
-scene.add(new THREE.AxesHelper(5));
 
 
 const debugObject = {
