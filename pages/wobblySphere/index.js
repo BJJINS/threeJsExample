9e6469fe0c0caff6ef5acd87d039da1f720e4b25
jinbaojia <bjjins@163.com>
2025-08-01 22:12:21 +0800

    feat(wobblySphere): 优化 wobble 效果并添加新功能
    - 重构 vertex 和 fragment shader，增加新的 uniforms 和逻辑
    - 添加颜色混合、roughness 变化等新效果
    - 优化 normal 计算，提高渲染质量
    - 增加 GUI 控制，方便调试参数
    - 优化代码结构，提高可读性和可维护性

--- pages/wobblySphere/index.js
+++ pages/wobblySphere/index.js
@@ -1,10 +1,11 @@
 import * as THREE from "three";
-import { scene, renderer, camera, gui } from "../template";
+import { scene, renderer, camera, gui, gltf } from "../template";
 import { OrbitControls, RGBELoader } from "three/examples/jsm/Addons.js";
 import environmentMapPath from "./static/urban_alley_01_1k.hdr?url";
 import CustomShaderMaterial from "three-custom-shader-material/vanilla";
-import wobbleVertexShader from "./shaders/wobble/vertex.glsl"
-import fragmentShader from "./shaders/wobble/fragment.glsl"
+import wobbleVertexShader from "./shaders/wobble/vertex.glsl";
+import wobbleFragmentShader from "./shaders/wobble/fragment.glsl";
+import { mergeVertices } from "three/examples/jsm/utils/BufferGeometryUtils.js";
 
 camera.fov = 35;
 camera.far = 100;
@@ -19,16 +20,39 @@ renderer.toneMappingExposure = 1;
 const orbitCOntrols = new OrbitControls(camera, renderer.domElement);
 orbitCOntrols.enableDamping = true;
 
+
+
 const rgbeLoader = new RGBELoader();
 rgbeLoader.load(environmentMapPath, (environmentMap) => {
     environmentMap.mapping = THREE.EquirectangularReflectionMapping;
     scene.background = environmentMap;
     scene.environment = environmentMap;
 });
+
+const debugObject = {};
+debugObject.colorA = '#0000ff';
+debugObject.colorB = '#ff0000';
+
+
+// Material
+const uniforms = {
+    uTime: new THREE.Uniform(0),
+    uPositionFrequency: new THREE.Uniform(0.5),
+    uTimeFrequency: new THREE.Uniform(0.4),
+    uStrength: new THREE.Uniform(0.3),
+
+    uWarpPositionFrequency: new THREE.Uniform(0.38),
+    uWarpTimeFrequency: new THREE.Uniform(0.12),
+    uWarpStrength: new THREE.Uniform(1.7),
+
+    uColorA: new THREE.Uniform(new THREE.Color(debugObject.colorA)),
+    uColorB: new THREE.Uniform(new THREE.Color(debugObject.colorB))
+};
+
 const material = new CustomShaderMaterial({
     baseMaterial: THREE.MeshPhysicalMaterial,
     vertexShader: wobbleVertexShader,
-    fragmentShader: fragmentShader,
+    fragmentShader: wobbleFragmentShader,
     // MeshPhysicalMaterial
     metalness: 0,
     roughness: 0.5,
@@ -37,19 +61,39 @@ const material = new CustomShaderMaterial({
     ior: 1.5,
     thickness: 1.5,
     transparent: true,
-    wireframe: false,
+    uniforms
 });
+
+gui.add(uniforms.uPositionFrequency, 'value', 0, 2, 0.001).name('uPositionFrequency');
+gui.add(uniforms.uTimeFrequency, 'value', 0, 2, 0.001).name('uTimeFrequency');
+gui.add(uniforms.uStrength, 'value', 0, 2, 0.001).name('uStrength');
+gui.add(uniforms.uWarpPositionFrequency, 'value', 0, 2, 0.001).name('uWarpPositionFrequency');
+gui.add(uniforms.uWarpTimeFrequency, 'value', 0, 2, 0.001).name('uWarpTimeFrequency');
+gui.add(uniforms.uWarpStrength, 'value', 0, 2, 0.001).name('uWarpStrength');
+
 gui.add(material, 'metalness', 0, 1, 0.001);
 gui.add(material, 'roughness', 0, 1, 0.001);
 gui.add(material, 'transmission', 0, 1, 0.001);
 gui.add(material, 'ior', 0, 10, 0.001);
 gui.add(material, 'thickness', 0, 10, 0.001);
-gui.addColor(material, 'color');
+gui.addColor(debugObject, 'colorA').onChange(() => uniforms.uColorA.value.set(debugObject.colorA));
+gui.addColor(debugObject, 'colorB').onChange(() => uniforms.uColorB.value.set(debugObject.colorB));
 // Geometry
-const geometry = new THREE.IcosahedronGeometry(2.5, 50);
+let geometry = new THREE.IcosahedronGeometry(2.5, 50);
+geometry = mergeVertices(geometry);
+geometry.computeTangents();
+
+const depthMaterial = new CustomShaderMaterial({
+    // CSM
+    baseMaterial: THREE.MeshDepthMaterial,
+    vertexShader: wobbleVertexShader,
+    depthPacking: THREE.RGBADepthPacking,
+    uniforms
+});
 
 // Mesh
 const wobble = new THREE.Mesh(geometry, material);
+wobble.customDepthMaterial = depthMaterial;
 wobble.receiveShadow = true;
 wobble.castShadow = true;
 scene.add(wobble);
@@ -69,7 +113,11 @@ directionalLight.shadow.camera.far = 15;
 directionalLight.shadow.normalBias = 0.05;
 directionalLight.position.set(0.25, 2, - 2.25);
 scene.add(directionalLight);
+
+const clock = new THREE.Clock();
 const tick = () => {
+    const elapsedTime = clock.getElapsedTime();
+    uniforms.uTime.value = elapsedTime;
     renderer.render(scene, camera);
     requestAnimationFrame(tick);
 };
