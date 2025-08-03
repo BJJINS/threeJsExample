ba00cffa4fc9fc324493b956fe62d77c50974313
jinbaojia <bjjins@163.com>
2025-08-03 23:09:28 +0800

    feat(fragmentShaderDemo): 添加矩形绘制功能并优化圆形绘制逻辑
    - 新增 plotRect 函数，用于绘制矩形图形，支持实心和空心选项
    - 优化 plotCircle 函数，提取公共代码提高代码复用性
    - 在 main 函数中添加矩形绘制示例

--- fragmentSahderDemo/显示数学公式.glsl
+++ fragmentSahderDemo/显示数学公式.glsl
@@ -25,14 +25,14 @@ float plotLine(vec2 st, float m, float b) {
 float plotCircle(vec2 st, vec2 center, float radius, bool isFilled) {
  // 计算当前点到圆心的距离
     float dist = length(st - center);
+    float outerEdge = 1.0 - smoothstep(radius - 0.01, radius + 0.01, dist);
     if(isFilled) {
         // 实心圆：距离小于半径的部分都填充
         // 使用smoothstep实现边缘抗锯齿
-        return 1.0 - smoothstep(radius - 0.01, radius + 0.01, dist);
+        return outerEdge;
     } else {
         // 空心圆：只保留距离在[radius-LINE_THICKNESS, radius]范围内的部分
         float inner = radius - LINE_THICKNESS;
-        float outerEdge = 1.0 - smoothstep(radius - 0.01, radius + 0.01, dist);
         float innerEdge = smoothstep(inner - 0.01, inner + 0.01, dist);
         return outerEdge * innerEdge;
     }
@@ -42,7 +42,27 @@ float plotCircle(vec2 st, vec2 center, float radius) {
     return plotCircle(st, center, radius, false);
 }
 
+float plotRect(vec2 st, vec2 leftBottom, vec2 rightTop, bool isFilled) {
+    // d 计算边界
+    // 如果st.x > leftBottom && st.x < rightTop 那么d.x必定是负数，反之则是正数
+    // y轴同理
+    // 这里取max是为了获取st的xy距离矩形最小的距离
+    vec2 d = max(leftBottom - st, st - rightTop);
+    float outDist = length(max(d, 0.0));
+    float outEdge = 1.0 - smoothstep(0.0, 0.01, outDist);
+    if(isFilled) {
+        // 如果st在矩形里面，则xy都是负数，distance是0
+        return outEdge;
+    } else {
+        float innerDist = abs(max(d.x, d.y));
+        float innerEdge = 1.0 - smoothstep(0.0, LINE_THICKNESS + 0.01, innerDist);
+        return innerEdge * outEdge;
+    }
+}
 
+float plotRect(vec2 st, vec2 leftBottom, vec2 rightTop) {
+    return plotRect(st, leftBottom, rightTop, false);
+}
 
 void main() {
     vec2 uv = gl_FragCoord.xy / u_resolution.xy;
@@ -63,9 +83,10 @@ void main() {
     color = mix(color, red, plot(st, y));
 
     float line = plotLine(st, 1.0, 0.0);
-    float circle = plotCircle(st, vec2(0.0, 0.0), 1.0, true);
+    float circle = plotCircle(st, vec2(0.0, 0.0), 1.0);
     color = mix(color, vec3(0.0, 1.0, 0.0), circle);
 
+    color = mix(color, vec3(0.0, 0.0, 1.0), plotRect(st, vec2(0.0), vec2(1.0)));
 
     gl_FragColor = vec4(color, 1.0);
 }
