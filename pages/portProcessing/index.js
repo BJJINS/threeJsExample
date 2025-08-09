ac869177cecbee4ca8c5ddc7bf887b8895552603
jinbaojia <bjjins@163.com>
2025-08-09 22:25:13 +0800

    Refactor code style and fix import naming in portProcessing

--- pages/portProcessing/index.js
+++ pages/portProcessing/index.js
@@ -1,22 +1,17 @@
-import * as THREE from 'three';
+import * as THREE from "three";
 import { scene, camera, renderer, gui, gltf, textureLoader } from "../template";
-import DamagedHelmetPath from "./static/models/DamagedHelmet/glTF/DamagedHelmet.gltf?url";
-import { OrbitControls } from 'three/examples/jsm/Addons.js';
+import damagedHelmetPath from "./static/models/DamagedHelmet/glTF/DamagedHelmet.gltf?url";
+import { OrbitControls } from "three/examples/jsm/Addons.js";
 
 gltf.useDraco();
 const cubeTextureLoader = new THREE.CubeTextureLoader();
-const environmentMap = cubeTextureLoader.setPath(import.meta.resolve("./static/textures/environmentMaps/0/")).load([
-    'px.jpg',
-    'nx.jpg',
-    'py.jpg',
-    'ny.jpg',
-    'pz.jpg',
-    'nz.jpg'
-]);
+const environmentMap = cubeTextureLoader
+    .setPath(import.meta.resolve("./static/textures/environmentMaps/0/"))
+    .load(["px.jpg", "nx.jpg", "py.jpg", "ny.jpg", "pz.jpg", "nz.jpg"]);
 
 scene.background = environmentMap;
 scene.environment = environmentMap;
-gltf.loader.load(DamagedHelmetPath, (gltf) => {
+gltf.loader.load(damagedHelmetPath, (gltf) => {
     gltf.scene.scale.set(2, 2, 2);
     gltf.scene.rotation.y = Math.PI * 0.5;
     scene.add(gltf.scene);
@@ -34,12 +29,12 @@ const updateAllMaterials = () => {
     });
 };
 
-const directionalLight = new THREE.DirectionalLight('#ffffff', 3);
+const directionalLight = new THREE.DirectionalLight("#ffffff", 3);
 directionalLight.castShadow = true;
 directionalLight.shadow.mapSize.set(1024, 1024);
 directionalLight.shadow.camera.far = 15;
 directionalLight.shadow.normalBias = 0.05;
-directionalLight.position.set(0.25, 3, - 2.25);
+directionalLight.position.set(0.25, 3, -2.25);
 scene.add(directionalLight);
 
 camera.position.set(4, 1, -4);
