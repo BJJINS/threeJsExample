8b636a58ea83f9fa73a1fb103d3f0316c0a866e9
jinbaojia <bjjins@163.com>
2025-08-10 12:39:11 +0800

    feat(portProcessing): 添加后期处理效果和着色器支持

--- pages/portProcessing/index.js
+++ pages/portProcessing/index.js
@@ -1,7 +1,9 @@
 import * as THREE from "three";
 import { scene, camera, renderer, gui, gltf, textureLoader, size } from "../template";
 import damagedHelmetPath from "./static/models/DamagedHelmet/glTF/DamagedHelmet.gltf?url";
-import { DotScreenPass, EffectComposer, GlitchPass, OrbitControls, RenderPass } from "three/examples/jsm/Addons.js";
+import { DotScreenPass, EffectComposer, GammaCorrectionShader, GlitchPass, OrbitControls, RenderPass, RGBShiftShader, ShaderPass, SMAAPass, UnrealBloomPass } from "three/examples/jsm/Addons.js";
+import vertexShader from "./vertex.glsl"
+import fragmentShader from "./fragment.glsl"
 
 gltf.useDraco();
 const cubeTextureLoader = new THREE.CubeTextureLoader();
@@ -45,12 +47,21 @@ renderer.shadowMap.type = THREE.PCFShadowMap;
 renderer.toneMapping = THREE.ReinhardToneMapping;
 renderer.toneMappingExposure = 1.5;
 
+const renderTarget = new THREE.WebGLRenderTarget(
+    800,
+    600,
+    {   // 采样数量，默认是0，采样数越大抗锯齿效果越好，性能越差
+        // 如果用户的像素比高于1，像素密度已经足够高，以至于无法分辨锯齿。在这种情况下，实际上不需要抗锯齿
+        // 兼容性有问题需要支持webgl2.0
+        samples: size.pixelRatio === 1 ? 2 : 0
+    }
+);
 // 效果合成器 后期处理
-const effectComposer = new EffectComposer(renderer);
+const effectComposer = new EffectComposer(renderer, renderTarget);
 effectComposer.setSize(size.width, size.height);
 effectComposer.setPixelRatio(size.pixelRatio);
 // 用来渲染原本的效果 等同于 renderer.render(scene, camera)
-const renderPass = new RenderPass(scene, camera); 
+const renderPass = new RenderPass(scene, camera);
 effectComposer.addPass(renderPass);
 
 const dotScreenPass = new DotScreenPass();
@@ -59,7 +70,47 @@ effectComposer.addPass(dotScreenPass);
 
 const glitchPass = new GlitchPass();
 glitchPass.enabled = false;
-effectComposer.addPass(glitchPass)
+effectComposer.addPass(glitchPass);
+
+const rgbShiftPass = new ShaderPass(RGBShiftShader);
+effectComposer.addPass(rgbShiftPass);
+const gammaCorrectionPass = new ShaderPass(GammaCorrectionShader);
+effectComposer.addPass(gammaCorrectionPass);
+
+const unrealBloomPass= new UnrealBloomPass();
+unrealBloomPass.strength = 0.3
+unrealBloomPass.radius = 1
+unrealBloomPass.threshold = 0.6
+unrealBloomPass.enabled = false;
+gui.add(unrealBloomPass, 'enabled')
+gui.add(unrealBloomPass, 'strength').min(0).max(2).step(0.001)
+gui.add(unrealBloomPass, 'radius').min(0).max(2).step(0.001)
+gui.add(unrealBloomPass, 'threshold').min(0).max(1).step(0.001)
+effectComposer.addPass(unrealBloomPass);
+
+const tintPass = new ShaderPass({
+    uniforms:{
+        // 上一阶段获取纹理
+        tDiffuse: { value: null }
+    },
+    vertexShader,
+    fragmentShader
+})
+
+effectComposer.addPass(tintPass)
+
+
+// 如果不支持webgl2.0，开启抗锯齿通道
+if (size.pixelRatio === 1 && !renderer.capabilities.isWebGL2) {
+    const smaaPass = new SMAAPass();
+    effectComposer.addPass(smaaPass);
+}
+
+
+window.addEventListener("resize", () => {
+    effectComposer.setSize(size.width, size.height);
+    effectComposer.setPixelRatio(size.pixelRatio);
+});
 
 const clock = new THREE.Clock();
 
