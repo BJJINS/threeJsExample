deaf292cea8271ae898949344bd0443a11a06994
jinbaojia <bjjins@163.com>
2025-09-04 00:05:52 +0800

    drive

--- fragmentSahderDemo/drive home.glsl
+++ fragmentSahderDemo/drive home.glsl
@@ -1,24 +1,25 @@
 struct ray {
-    vec3 o;
-    vec3 d;
+    vec3 origin; // 射线原点相机位置
+    vec3 direction; // 射线方向
 };
 
+// 光线追踪
 ray getRay(vec2 uv, vec3 camPos, vec3 lookAt, float room) {
     ray a;
-    a.o = camPos;
+    a.origin = camPos;
 
-    vec3 f = normalize(lookAt - camPos);
-    vec3 r = cross(vec3(0.0, 1.0, 0.0), f);
-    vec3 u = cross(f, r);
-    vec3 c = a.o + f * room;
-    vec3 i = c + uv.x * r + uv.y * u;
-    a.d = normalize(i - a.o);
+    vec3 forward = normalize(lookAt - camPos);
+    vec3 right = cross(forward, vec3(0.0, 1.0, 0.0));
+    vec3 up = cross(right, forward);
+    vec3 c = a.origin + forward * room; // 将相机位置沿前方向移动 room 距离，得到近平面中心坐标
+    vec3 i = c + uv.x * right + uv.y * up; // 计算近平面上的采样点（射线目标点）
+    a.direction = normalize(i - a.origin);
 
     return a;
 }
 
 vec3 closestPoint(ray r, vec3 p) {
-    return r.o + max(0.0, dot(p - r.o, r.d)) * r.d;
+    return r.origin + max(0.0, dot(p - r.origin, r.direction)) * r.direction;
 }
 
 float distRay(ray r, vec3 p) {
@@ -28,15 +29,16 @@ float distRay(ray r, vec3 p) {
 void main() {
     vec2 uv = gl_FragCoord.xy / iResolution.xy;
     uv -= 0.5;
+    uv *= 2.0;
     uv.x *= iResolution.x / iResolution.y;
 
-    vec3 camPos = vec3(0.0, 0.2, 0.0);
-    vec3 lookAt = vec3(0.0, 0.2, 1.0);
-    ray r = getRay(uv, camPos, lookAt, 1.0);
+    vec3 camPos = vec3(0.0, 0.0, 0.0);
+    vec3 lookAt = vec3(0.0, 0.0, -1.0);
+    ray r = getRay(uv, camPos, lookAt, 2.0);
 
-    vec3 p = vec3(0.0, 0.0, 5.0);
+    vec3 p = vec3(2.5, 0.0, -5.0);
     float d = distRay(r, p);
-    // float c = smoothstep(0.1, 0.09, d);
+    float c = smoothstep(0.1, 0.09, d);
 
-    gl_FragColor = vec4(d);
+    gl_FragColor = vec4(c);
 }
