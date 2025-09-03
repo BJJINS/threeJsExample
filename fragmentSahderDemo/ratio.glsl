b4513df9682803fb47930a96be738e3206bfbd7e
jinbaojia <bjjins@163.com>
2025-09-04 01:05:29 +0800

    删除文件

--- fragmentSahderDemo/ratio.glsl
+++
@@ -1,7 +0,0 @@
-vec2 ratio(in vec2 v, in vec2 s) {
-    return mix(
-        vec2(v.x * s.x / s.y - (s.x - s.y) * 0.5 / s.y, v.y),
-        vec2(v.x, v.y * s.y / s.x - (s.y - s.x) * 0.5 / s.x),
-        step(s.x, s.y)
-    );
-}
